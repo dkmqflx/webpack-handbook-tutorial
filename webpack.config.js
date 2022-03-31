1479f1ef48714ca0bf84dfb0e942a7701f26aaea
dkmqflx <dkmqflx@gmail.com>
2022-03-31 17:04:14 +0900

    Code splitting 실습

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
-// webpack.config.js
-// `webpack` command will pick up this config setup by default
 var path = require('path');
 
 module.exports = {
   mode: 'none',
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
